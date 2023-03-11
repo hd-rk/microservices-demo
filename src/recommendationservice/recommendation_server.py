9641bee6fee0416d6cdbfff80dfecd18985a255e
SiyuAn166 <siyu.an166@gmail.com>
2023-03-11 12:30:30 -0800

    Cache recommendations

--- src/recommendationservice/recommendation_server.py
+++ src/recommendationservice/recommendation_server.py
@@ -34,6 +34,7 @@ from opentelemetry.instrumentation.grpc import GrpcInstrumentorClient, GrpcInstr
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+import redis, time, threading
 
 from logger import getJSONLogger
 logger = getJSONLogger('recommendationservice-server')
@@ -64,23 +65,54 @@ def initStackdriverProfiling():
   return
 
 class RecommendationService(demo_pb2_grpc.RecommendationServiceServicer):
+    def __init__(self):
+        self.cache = redis.Redis(host=os.environ.get('REDIS_ADDR', 'localhost'), port=6379, db=0)
+        self.max_responses = 5
+        self.cache_expiry = 300  # 5 minutes in seconds
+        self.last_cache_update = 0
+        # start background thread to update cache periodically
+        self.cache_thread = threading.Thread(target=self.update_cache)
+        self.cache_thread.daemon = True
+        self.cache_thread.start()
+      
+    def update_cache(self):
+      while True:
+          # fetch list of products from product catalog stub
+          cat_response = product_catalog_stub.ListProducts(demo_pb2.Empty())
+          product_ids = [x.id for x in cat_response.products]
+          top_products = product_ids[:5]
+          self.cache.set('top_products', ','.join(top_products))
+          self.last_cache_update = time.time()
+          time.sleep(self.cache_expiry)
+
     def ListRecommendations(self, request, context):
-        max_responses = 5
-        # fetch list of products from product catalog stub
-        cat_response = product_catalog_stub.ListProducts(demo_pb2.Empty())
-        product_ids = [x.id for x in cat_response.products]
-        filtered_products = list(set(product_ids)-set(request.product_ids))
-        num_products = len(filtered_products)
-        num_return = min(max_responses, num_products)
-        # sample list of indicies to return
-        indices = random.sample(range(num_products), num_return)
-        # fetch product ids from indices
-        prod_list = [filtered_products[i] for i in indices]
-        logger.info("[Recv ListRecommendations] product_ids={}".format(prod_list))
-        # build and return response
-        response = demo_pb2.ListRecommendationsResponse()
-        response.product_ids.extend(prod_list)
-        return response
+      # retrieve top 5 products from cache
+      if not self.cache.exists('top_products'):
+          self.update_cache()
+      top_products = self.cache.get('top_products').decode().split(',')
+      logger.info("[Recv ListRecommendations] product_ids={}".format(top_products))
+      # build and return response
+      response = demo_pb2.ListRecommendationsResponse()
+      response.product_ids.extend(top_products)
+      return response
+
+    # def ListRecommendations(self, request, context):
+    #     max_responses = 5
+    #     # fetch list of products from product catalog stub
+    #     cat_response = product_catalog_stub.ListProducts(demo_pb2.Empty())
+    #     product_ids = [x.id for x in cat_response.products]
+    #     filtered_products = list(set(product_ids)-set(request.product_ids))
+    #     num_products = len(filtered_products)
+    #     num_return = min(max_responses, num_products)
+    #     # sample list of indicies to return
+    #     indices = random.sample(range(num_products), num_return)
+    #     # fetch product ids from indices
+    #     prod_list = [filtered_products[i] for i in indices]
+    #     logger.info("[Recv ListRecommendations] product_ids={}".format(prod_list))
+    #     # build and return response
+    #     response = demo_pb2.ListRecommendationsResponse()
+    #     response.product_ids.extend(prod_list)
+    #     return response
 
     def Check(self, request, context):
         return health_pb2.HealthCheckResponse(
