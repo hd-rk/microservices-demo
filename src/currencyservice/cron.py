09a3e696312c3bf0df7deb492ce5fc893030cba8
hd-rk <hdn314159@gmail.com>
2023-03-11 22:13:34 -0800

    feat: k8s for currency service

--- src/currencyservice/cron.py
+++ src/currencyservice/cron.py
@@ -8,12 +8,25 @@ DEFAULT_CURRENCY = os.environ.get('DEFAULT_CURRENCY')
 CURRENCY_SUPPORT = os.environ.get('CURRENCY_SUPPORT').split(',')
 
 # r = redis.Redis(host='localhost', port=6379, db=0)
-r = redis.Redis(host=os.environ.get('REDIS_HOST'), port=int(os.environ.get('REDIS_PORT')), db=0)
+r = redis.Redis(
+    host=os.environ.get('REDIS_HOST'),
+    port=int(os.environ.get('REDIS_PORT')),
+    db=0
+)
 
-payload = {DEFAULT_CURRENCY:1}
+payload = {
+    DEFAULT_CURRENCY: 1
+}
 
 for currency in CURRENCY_SUPPORT:
-    response = requests.get(f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={DEFAULT_CURRENCY}&to_currency={currency}&apikey=DEO388ZM3UEZ34M8')
+    response = requests.get(
+        "https://www.alphavantage.co/query",
+        params={
+            "function": "CURRENCY_EXCHANGE_RATE",
+            "from_currency": DEFAULT_CURRENCY,
+            "to_currency": currency,
+            "api_key": APIKEY
+        })
     response_dict = response.json()
     first_key = list(response_dict)[0]
     fifth_key = list(response_dict[first_key])[4]
