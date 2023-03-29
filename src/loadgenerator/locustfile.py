cc969769eb7202e08e8158770a75e0ddb2916aee
Alvin Lam <alvin@locbit.com>
2023-03-28 22:49:49 -0700

    LoadGenerator change

--- src/loadgenerator/locustfile.py
+++ src/loadgenerator/locustfile.py
@@ -16,6 +16,7 @@
 
 import random
 from locust import HttpUser, TaskSet, between
+import logging
 
 products = [
     '0PUK6V6EV0',
@@ -28,26 +29,39 @@ products = [
     'LS4PSXUNUM',
     'OLJCESPC7Z']
 
+
+
 def index(l):
-    l.client.get("/")
+    response = l.client.get("/")
+    if l.cookies is None:
+        l.cookies = response.cookies
+    logging.info(f"loading landing page with cookie: {l.cookies['shop_session-id']}")
 
 def setCurrency(l):
-    currencies = ['EUR', 'USD', 'JPY', 'CAD']
+    logging.info(f"loading setCurrenct page with cookie: {l.cookies['shop_session-id']}")
+    currencies = ['EUR', 'USD', 'JPY', 'CAD', 'GBP', 'TRY']
     l.client.post("/setCurrency",
-        {'currency_code': random.choice(currencies)})
+        {'currency_code': random.choice(currencies)},
+        cookies=l.cookies)
 
 def browseProduct(l):
-    l.client.get("/product/" + random.choice(products))
+    l.client.get("/product/" + random.choice(products),
+        cookies=l.cookies)
 
 def viewCart(l):
-    l.client.get("/cart")
+    l.client.get("/cart",
+        cookies=l.cookies)
 
 def addToCart(l):
     product = random.choice(products)
-    l.client.get("/product/" + product)
-    l.client.post("/cart", {
-        'product_id': product,
-        'quantity': random.choice([1,2,3,4,5,10])})
+    l.client.get("/product/" + product,
+        cookies=l.cookies)
+    l.client.post("/cart", 
+        {
+            'product_id': product,
+            'quantity': random.choice([1,2,3,4,5,10])
+        },
+        cookies=l.cookies)
 
 def checkout(l):
     addToCart(l)
@@ -62,11 +76,12 @@ def checkout(l):
         'credit_card_expiration_month': '1',
         'credit_card_expiration_year': '2039',
         'credit_card_cvv': '672',
-    })
+    }, cookies=l.cookies)
 
 class UserBehavior(TaskSet):
 
     def on_start(self):
+        self.cookies = None
         index(self)
 
     tasks = {index: 1,
